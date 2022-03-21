4b933b95f69e4c7ee962a191b5f1ed1c3068f237
yamanekaito0630 <yamanekaito0630@gmail.com>
2022-03-21 15:18:38 +0900

    chenge login layouts

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,4 +11,22 @@
     >
         <span class="navbar-toggler-icon"></span>
     </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            @auth
+                <li class="nav-item">
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </li>
+            @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+            @endauth
+        </ul>
+    </div>
 </header>
