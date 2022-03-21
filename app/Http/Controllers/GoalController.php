6c7696465f9d07e840dbb56347592a5c924dfbaa
yamanekaito0630 <yamanekaito0630@gmail.com>
2022-03-21 15:27:35 +0900

    change GoalController

--- app/Http/Controllers/GoalController.php
+++ app/Http/Controllers/GoalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Goal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GoalController extends Controller
 {
@@ -14,17 +15,9 @@ class GoalController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $goals = Auth::user()->goals;
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return response()->json($goals);
     }
 
     /**
@@ -35,29 +28,14 @@ class GoalController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $goal = new Goal();
+        $goal->title = request('title');
+        $goal->user_id = Auth::id();
+        $goal->save();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Goal  $goal
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Goal $goal)
-    {
-        //
-    }
+        $goals = Auth::user()->goals;
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Goal  $goal
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Goal $goal)
-    {
-        //
+        return response()->json($goals);
     }
 
     /**
@@ -69,7 +47,13 @@ class GoalController extends Controller
      */
     public function update(Request $request, Goal $goal)
     {
-        //
+        $goal->title = request('title');
+        $goal->user_id = Auth::id();
+        $goal->save();
+
+        $goals = Auth::user()->goals;
+
+        return response()->json($goals);
     }
 
     /**
@@ -80,6 +64,10 @@ class GoalController extends Controller
      */
     public function destroy(Goal $goal)
     {
-        //
+        $goal->delete();
+
+        $goals = Auth::user()->goals;
+
+        return response()->json($goals);
     }
 }
